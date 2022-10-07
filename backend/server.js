3df27890b45586e9329937e23d5415855f2fe5e4
Your Name <you@example.com>
2022-10-07 10:34:13 +0200

    single-product-details

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable linebreak-style */
 import express from 'express';
 import cors from 'cors';
@@ -9,6 +10,15 @@ app.use(cors());
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
+app.get('/api/products/:id', (req,res) =>{
+  const product = data.products.find((x) =>x._id === req.params.id);
+  if(product){
+    res.send(product)
+  } else{
+    res.status(404).send({message: 'Product Not Found!'})
+  }
+
+})
 
 app.listen(5000, () => {
   console.log('serve at http://localhost:5000');
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,16 @@
+/* eslint-disable arrow-body-style */
+import { getProduct } from '../api';
+import {parseRequestUrl} from '../utils';
+
 const productScreen = {
-    render: () =>{
-        return `<div>ProductScreen</div>`
-    },
+  render: async () => {
+    const request = parseRequestUrl();
+    const product = await getProduct(request.id);
+    if(product.error){
+        return `<div>${product.error}</div>`
+    }
+    return `<h1>${product.name}</h1>`
+  },
 };
 
 export default productScreen;
